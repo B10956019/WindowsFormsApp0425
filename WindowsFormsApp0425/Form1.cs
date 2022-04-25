c7d7e230b30af612f9223323585c7ac16f82aaa9
user <user@DESKTOP-6JBEF5K>
2022-04-25 11:04:19 +0800

    1

---
+++ WindowsFormsApp0425/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+
+namespace WindowsFormsApp0425
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TcpListener Server;
+        Socket Client;
+        Thread Th_Svr;
+        Thread Th_Clt;
+        Hashtable ET = new Hashtable();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Th_Svr = new Thread(ServerSub);
+            Th_Svr.IsBackground = true;
+            Th_Svr.Start();
+            button1.Enabled = false;
+        }
+        private void ServerSub()
+        {
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+
+            Server = new TcpListener(EP);
+            Server.Start(100);
+            while (true)
+            {
+                Client = Server.AcceptSocket();
+                Th_Clt = new Thread(Listen);
+                Th_Clt.IsBackground = true;
+                Th_Clt.Start();
+            }
+        }
+        private void Listen()
+        {
+            Socket Sck = Client;
+            Thread TH = Th_Clt;
+            while (true)
+            {
+                try
+                {
+                    byte[] B = new byte[1023];
+
+                }
+            }
+        }
+    }
+}
